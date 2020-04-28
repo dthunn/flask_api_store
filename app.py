aafbe706d32a7972549d012aa388e58aeeb23ca9
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2020-04-28 16:00:59 -0500

    added run.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "super-secret"
 api = Api(app)
-db.init_app(app)
 
 
 @app.before_first_request
@@ -31,4 +30,5 @@ api.add_resource(StoreList, "/stores")
 
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=True)
